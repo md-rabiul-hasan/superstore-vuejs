88b927ae02697fcf6bbcc1548534df779f235cbb
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-02-28 00:14:14 +0600

    single file component

---
+++ src/components/Cart.vue
@@ -0,0 +1,28 @@
+<template>
+      <div class="cart-list">
+            <ul class="list-group">
+              <li class="list-group-item active" aria-current="true">Cart</li>
+            </ul>
+            <br>
+            <ul class="list-group">
+              <li class="list-group-item " aria-current="true">Cart</li>
+              <li class="list-group-item " aria-current="true">Cart</li>
+              <li class="list-group-item " aria-current="true">Cart</li>
+              <li class="list-group-item " aria-current="true">Cart</li>
+            </ul>
+            <br>
+            <ul class="list-group">
+              <li class="list-group-item active" aria-current="true">Total</li>
+            </ul>
+          </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/components/Inventory.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="row">
+              <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                  <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                  <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                  <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    
+</style>
