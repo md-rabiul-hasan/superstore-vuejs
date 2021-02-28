9f4948c9e36ef290592735a9430c671a2d522896
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-03-01 02:17:32 +0600

    fetching details

--- src/components/views/ItemDetails.vue
+++ src/components/views/ItemDetails.vue
@@ -1,10 +1,40 @@
 <template>
-    <h4>Details - {{ $route.params.id }}</h4>
+    <div class="row" v-if="item_found">
+        <div class="col-md-6">
+            <img :src="item.photo" alt="">
+        </div>
+        <div class="col-md-6">
+            <h4>{{ item.title }}</h4>
+            <p>{{ item.description }}</p>
+            <h6>$ {{ item.price }}</h6>
+        </div>
+    </div>
+    <h5 v-else>Loading ..... </h5>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    
+    data(){
+        return{
+            item : null,
+            item_found : false
+        }
+    },
+    mounted(){
+        this.itemDetails()
+    },
+    methods:{
+        itemDetails(){
+            var self = this
+            axios.get('http://localhost:3000/item/' + this.$route.params.id).then(response => {
+                setTimeout(function(){
+                    self.item_found = true
+                    self.item = response.data
+                }, 1000)                
+            })
+        }
+    }
 }
 </script>
 
