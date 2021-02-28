bf99d7866735a23bcda1efd2e8353dea92c48a07
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-03-01 01:16:36 +0600

    item details page create

---
+++ src/components/views/ItemDetails.vue
@@ -0,0 +1,14 @@
+<template>
+    <h4>Details - {{ $route.params.id }}</h4>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Hello from '@/components/views/Hello'
 import Test from '@/components/views/Test'
 import Inventory from '@/components/views/Inventory'
+import ItemDetails from '@/components/views/ItemDetails'
 
 export const routes = [
     { path : '', component : Inventory },
-    { path: '/test/:id', component : Test}
+    { path: '/item/:id', component : ItemDetails}
 ]
