1f2d037a533c251487eb2cf6d691c0234e2572b5
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-03-01 02:01:05 +0600

    fetching inventory

--- src/components/views/Inventory.vue
+++ src/components/views/Inventory.vue
@@ -1,28 +1,45 @@
 <template>
-    <div class="row">
+    <div class="row"  v-if="!loading">
         <div v-for="(item,index) in items" :key="index" class="col-md-4 mb-5">
-        <div class="card" style="width: 18rem;">
-            <img :src="item.photo" class="card-img-top img-style" >
-            <div class="card-body">
-            <h5 class="card-title">{{item.title}}</h5>
-            <p class="card-text">{{ item.price }}</p>
-            <a @click="addToCart(item)" class="btn btn-primary">+add</a>
+            <div class="card" style="width: 18rem;">
+                <img :src="item.photo" class="card-img-top img-style" >
+                <div class="card-body">
+                <h5 class="card-title">{{item.title}}</h5>
+                <p class="card-text">{{ item.price }}</p>
+                <a @click="addToCart(item)" class="btn btn-primary">+add</a>
+                </div>
             </div>
         </div>
-        </div>
     </div>
+    <h6 v-else>Loading items ....</h6>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data(){
         return {
-            items : []
+            items : [],
+            loading : true
         }
+    },
+    mounted(){
+        this.fetchItems()
     },
     methods : {
         addToCart(item){
             this.$emit('newItemAdded', item);
+        },
+        fetchItems(){
+            var self = this
+            axios.get('http://localhost:3000/items').then(response => {
+                setTimeout(function(){
+                    self.items = response.data,
+                    self.loading = false
+                }, 3000)
+               
+            })
         }
     }
 }
